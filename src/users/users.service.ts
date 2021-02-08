efd07ed83aa4e2b52a7b91f1aac764139aba48ba
Amir Sajjad Poursoltani <sajjadpooresq@gmail.com>
2021-02-08 09:29:45 +0330

    add user service

---
+++ src/users/users.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './users.model.ts';
+import { uniqid } from 'uniqid';
+
+@Injectable()
+export class UsersService {
+  users: User[] = [];
+
+  addUser(name: string, username: string, password: string, email: string) {
+    const id = uniqid();
+    const newUser = new User(id, name, username, password, email);
+    this.users.push(newUser)
+  }
+}
